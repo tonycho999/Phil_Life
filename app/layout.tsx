5cc379013704ef697d63fd27da4fefa56f05ca37
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-02-27 07:56:17 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css"; // ★ 이 파일이 app 폴더 안에 있어야 스타일이 먹힙니다!
+import "@/globals.css"; // ★ 이 파일이 app 폴더 안에 있어야 스타일이 먹힙니다!
 import Header from "@/components/layout/Header";
 
 const inter = Inter({ subsets: ["latin"] });
