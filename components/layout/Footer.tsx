3704de7ac5226c0216e3e6ea96e960decc664ef2
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-03-13 15:17:33 +0800

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -34,7 +34,7 @@ export default function Footer() {
         <div className="text-xs space-y-2 text-center text-gray-400">
           <p>
             <span className="font-bold text-gray-500">필카페24 (PhilCafe24)</span> 
-
+          </p>
           <p>
             본 사이트에 등록된 모든 콘텐츠는 저작권법의 보호를 받으며, 무단 전재, 복사, 배포 등을 금합니다.<br className="hidden md:block" />
             게시판에 작성된 게시물의 저작권과 책임은 작성자 본인에게 있습니다.
