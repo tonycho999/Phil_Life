25d9607cfd2434bf9628e80002ddd08345be193e
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-04-06 14:29:54 +0800

    Update AdBannerRight.tsx

--- components/layout/AdBannerRight.tsx
+++ components/layout/AdBannerRight.tsx
@@ -1,12 +1,18 @@
 // components/layout/AdBannerRight.tsx
 export default function AdBannerRight() {
   return (
-    <div className="hidden lg:block relative">
-      <div className="sticky top-40 w-full h-[600px] bg-gray-200/60 border border-gray-300 rounded-lg flex items-center justify-center text-gray-500 font-bold shadow-inner transition-colors hover:bg-gray-200">
-        <span className="text-center">
-          우측 배너<br />
-          <span className="text-xs font-normal">160x600 등</span>
-        </span>
+    <div className="hidden lg:block relative w-[160px]">
+      {/* sticky를 유지하여 스크롤 시에도 광고가 따라다니게 합니다. */}
+      <div className="sticky top-40 w-[160px] h-[600px] flex items-center justify-center bg-white rounded-lg overflow-hidden shadow-sm border border-gray-200">
+        <iframe
+          src="https://ads-partners.coupang.com/widgets.html?id=977715&template=carousel&trackingCode=AF7694250&subId=&width=160&height=600"
+          width="160"
+          height="600"
+          frameBorder="0"
+          scrolling="no"
+          referrerPolicy="unsafe-url"
+          className="border-none bg-transparent"
+        ></iframe>
       </div>
     </div>
   );
